183f86e76e726dce27d4285453cacf1c3576e670
git136975643 <63916062+git136975643@users.noreply.github.com>
2022-07-08 12:19:14 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'https://git136975643.github.io/peace-elite/'
+      url.hostname = 'git136975643.github.io/peace-elite/'
       // url.hostname = '136975643.workers.dev'
       let new_request = new Request(url, request);
       return fetch(new_request);
